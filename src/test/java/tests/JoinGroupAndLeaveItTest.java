f1df44be6d446227668c7767325dd10e3f381f68
IlyaAAAA <ilya-sachuk@mail.ru>
2021-11-02 16:38:59 +0300

    fixed

--- src/test/java/tests/JoinGroupAndLeaveItTest.java
+++ src/test/java/tests/JoinGroupAndLeaveItTest.java
@@ -1,11 +1,14 @@
 package tests;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.groupPage.GroupPage;
 import pages.groupsPage.Group;
 import pages.groupsPage.GroupsPage;
 import pages.mainPage.MainPage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class JoinGroupAndLeaveItTest extends BasicTest {
     @Test
@@ -14,11 +17,19 @@ public class JoinGroupAndLeaveItTest extends BasicTest {
                 .getLeftNavigationBar()
                 .openGroups();
 
+        int groupsNumberBeforeJoin = groupsPage
+                .getMyGroups()
+                .getGroupsNumber();
+
         Group newGroup = groupsPage
                 .getRecommendedGroups()
                 .clickJoinFirstGroup();
 
         groupsPage.reloadPage();
+
+        int groupsNumberAfterJoin = groupsPage
+                .getMyGroups()
+                .getGroupsNumber();
 
         GroupPage groupPage = groupsPage
                 .getMyGroups()
@@ -28,10 +39,21 @@ public class JoinGroupAndLeaveItTest extends BasicTest {
 
         groupPage.leave();
 
-        Assertions.assertFalse(openMainPage()
+        int groupsNumberAfterLeave = openMainPage()
                 .getLeftNavigationBar()
                 .openGroups()
                 .getMyGroups()
-                .isInMyGroups(newGroup));
+                .getGroupsNumber();
+
+//        Assertions.assertFalse(openMainPage()
+//                .getLeftNavigationBar()
+//                .openGroups()
+//                .getMyGroups()
+//                .isInMyGroups(newGroup));
+
+        assertThat(groupsNumberBeforeJoin, Matchers.lessThan(groupsNumberAfterJoin));
+        assertThat(groupsNumberAfterLeave, Matchers.lessThan(groupsNumberAfterJoin));
+        assertThat(groupsNumberBeforeJoin, Matchers.comparesEqualTo(groupsNumberAfterLeave));
+
     }
 }
